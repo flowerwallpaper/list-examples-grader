124ea07cec2ea793dbf183fa5f9daa4791690a56
Thotticus <koalalove4me@gmail.com>
2022-11-17 12:35:02 -0800

    update from week 7

--- grade.sh
+++ grade.sh
@@ -5,3 +5,20 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+cp TestListExamples.java
+
+cd student-submission
+
+cat TestListExamples.java
+
+if [ -f "ListExamples.java" ]
+then 
+    echo "File submitted!"
+else 
+    echo "ListExamples.java not submitted" 
+fi
+
+javac ListExamples.java TestListExamples.java
+java ListExamples TestListExamples
+
+
